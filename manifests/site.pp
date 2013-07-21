95271b393d7c09169299864e9dea4760749df017
berrak <bkronmailbox-git@yahoo.se>
2013-07-21 19:44:08 +0200

    Add new host hard - a hardened server VM

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,21 @@
 #
 # site.pp (Virtualbox VM's)
 #
+## Purpose is to build a hardened server host
+node 'node-hard.vbox.tld' {
+    
+    include vb_hosts
+    
+    include vb_puppetize
+    
+    include vb_root_home
+    
+    include vb_root_bashrc
+    
+    # Replace 'bekr' with your existing username
+    vb_user_bashrc::config { 'bekr' : }
+    
+}
 ## Purpose is to build a isolated hardened host for a safer web experience
 node 'node-web.vbox.tld' {
     
