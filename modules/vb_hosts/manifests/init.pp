2ffd702fee8d68656c2bcb45812dec4ee5645100
berrak <bkronmailbox-git@yahoo.se>
2013-07-30 14:57:22 +0200

    Erb pass syntax check

--- modules/vb_hosts/manifests/init.pp
+++ modules/vb_hosts/manifests/init.pp
@@ -8,11 +8,13 @@ define vb_hosts ( $apache_virtual_host = '') {
 	$mydomain = $::domain
 	
 	if $apache_virtual_host != '' {
-        $vhostfqdn = "$::ipaddress  $apache_virtual_host"
+        $vhostfqdn = $apache_virtual_host
     } else
 	{
 	    $vhostfqdn = ''
 	}
+
+    $hostip = $::ipaddress 
 
     file { "/etc/hosts" :
         content => template( "vb_hosts/hosts.erb" ),
