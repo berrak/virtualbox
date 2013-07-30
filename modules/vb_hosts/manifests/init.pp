324e02b8713421bffe6f00e42604116b8d3e5b81
berrak <bkronmailbox-git@yahoo.se>
2013-07-30 13:49:49 +0200

    Try with parameter to define

--- modules/vb_hosts/manifests/init.pp
+++ modules/vb_hosts/manifests/init.pp
@@ -1,13 +1,13 @@
 ##
 ## This define manage the settings in hosts file
 ##
-define vb_hosts {
+define vb_hosts ( $apache_virtual_host = '') {
 
 	$myaddress = $::ipaddress
 	$myhostname = $::hostname
 	$mydomain = $::domain
 	
-    $teststring = ''
+
 
     file { "/etc/hosts" :
         content => template( "vb_hosts/hosts.erb" ),
