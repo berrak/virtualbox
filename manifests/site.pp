d858433844f9aff4bdb93b521e7f5e69e0b22b4e
berrak <bkronmailbox-git@yahoo.se>
2013-07-30 14:28:47 +0200

    Dbg: Wrong header format - usully syntax errors in template

--- manifests/site.pp
+++ manifests/site.pp
@@ -71,7 +71,7 @@ node 'node-php.vbox.tld' inherits basenode {
     vb_apache2::vhost { 'hudson.vbox.tld' : priority => '001' }
     
     # Manage /etc/hosts file, also Apache virtual hosts
-    vb_hosts { 'php.vbox.tld' : apache_virtual_host => ['hudson.vbox.tld', 'test.vbox.tld'] }    
+    vb_hosts { 'php.vbox.tld' : apache_virtual_host => 'hudson.vbox.tld'}    
 
 }
 
--- modules/vb_hosts/manifests/init.pp
+++ modules/vb_hosts/manifests/init.pp
@@ -7,7 +7,12 @@ define vb_hosts ( $apache_virtual_host = '') {
 	$myhostname = $::hostname
 	$mydomain = $::domain
 	
-
+	if $apache_virtual_host != '' {
+        $vhostfqdn = "$::ipaddress  $apache_virtual_host"
+    } else
+	{
+	    $vhostfqdn = ''
+	}
 
     file { "/etc/hosts" :
         content => template( "vb_hosts/hosts.erb" ),
