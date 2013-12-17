bfa5f9168c475a553d5d70e40c95c09584d4704b
berrak <bkronmailbox-git@yahoo.se>
2013-12-17 20:04:13 +0100

    Disable default site

--- modules/vb_apache2/manifests/vhost.pp
+++ modules/vb_apache2/manifests/vhost.pp
@@ -85,6 +85,12 @@ define vb_apache2::vhost ( $priority='', $devgroupid='', $urlalias='', $aliastgt
                 command => "/usr/sbin/a2enmod suexec",
             }
             
+            # When we use cgi/jensen we must disable 'default' vhost, beacuse
+            # apache looks for a matching URL in 'default' before 'jensen!'
+            
+            exec { "disable_site_default":
+                 command => "/usr/sbin/a2dissite default"
+            }
                 
             
             file { "/etc/apache2/sites-available/${name}":
