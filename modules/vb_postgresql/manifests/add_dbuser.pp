871d05e37225636e1a07bdd9990b00cce5b2794f
berrak <bkronmailbox-git@yahoo.se>
2013-12-17 22:03:57 +0100

    Syntax

--- modules/vb_postgresql/manifests/add_dbuser.pp
+++ modules/vb_postgresql/manifests/add_dbuser.pp
@@ -11,7 +11,7 @@ define vb_postgresql::add_dbuser {
 		
 	# add one database user
 	
-	exec { "create_postgres_user":
+	exec { "create_postgres_user ":
 		command => "/bin/su - postgres && /usr/bin/createuser $name --createdb --no-superuser --no-password --no-createrole && /bin/sh exit",
 		require => Class["vb_postgresql"],
 		notify => "set_restrictive_mode_on_user_postgresql_password_file",
@@ -19,7 +19,7 @@ define vb_postgresql::add_dbuser {
 	
 	# although the user password may not be in use, it should always have mode '0600'
 	
-    exec { "set_restrictive_mode_on_user_postgresql_password_file":
+    exec { "set_restrictive_mode_on_user_postgresql_password_file" :
          command => "/bin/chmod 0600 /home/$name/.pgpass",    
     }
 	
