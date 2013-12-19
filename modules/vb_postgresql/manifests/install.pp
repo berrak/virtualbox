2fa3591acaf905c0efd55dd81bb3e1befb61caa6
berrak <bkronmailbox-git@yahoo.se>
2013-12-19 09:11:11 +0100

    Add postgresql development files

--- modules/vb_postgresql/manifests/install.pp
+++ modules/vb_postgresql/manifests/install.pp
@@ -3,8 +3,9 @@
 #
 class vb_postgresql::install {
 
-    package { "postgresql"        : ensure => installed }
-    package { "postgresql-client" : ensure => installed }
-    package { "pgadmin3"          : ensure => installed }
+    package { "postgresql"                : ensure => installed }
+    package { "postgresql-client"         : ensure => installed }
+    package { "pgadmin3"                  : ensure => installed }
+    package { "postgresql-server-dev-9.1" : ensure => installed }    
 
 }
